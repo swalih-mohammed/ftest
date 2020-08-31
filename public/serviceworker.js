0fe05d6d487ec749fc0303c87dde6b06c6a507b6
sibiyan <sibiyan9030@gmail.com>
2020-08-31 21:09:34 +0530

    pwaerr

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,5 +1,6 @@
 const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const urlsToCache = ["index.html", "offline.html"];
+const urlsToCache = ["offline.html"];
 
 const self = this;
 
